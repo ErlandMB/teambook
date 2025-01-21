e7652c7b3cf19657b7a7798b2a47b9961038d2d6
ErlandMB <mauricioerland02@gmail.com>
2025-01-21 00:09:05 -0400

    Update Coeficiente binomial.cpp

--- math/Coeficiente binomial.cpp
+++ math/Coeficiente binomial.cpp
@@ -1,15 +1,18 @@
-struct bin_coef {
-	vector<vi> dp;
-	int mod;
-	bin_coef(int maxN, int maxK, int mod) : dp(maxN+1, vi(maxK+1, -1)), mod(mod) {}
-	int nck(int n, int k) {
-		if (n < k) return 0;
-		if (n == k || k == 0) return 1;
-		if (k == 1) return n; // n>=mod ?
-		int &res = dp[n][k];
-		if (res != -1) return res;
-		res = nck(n-1, k) + nck(n-1, k-1);
-		res = (res >= mod ? res-mod : res);
-		return res;
-	}
+ll expmod(ll x, ll n) {if (n == 0) return 1 % MOD; ll u = expmod(x, n / 2); u = (u * u) % MOD; if (n & 1) u = (u * x) % MOD; return u; }
+
+ll invmod(ll a){ return expmod(a, MOD - 2); }
+
+const int max_n = int(2e5 + 1);
+
+vll fac(max_n), fac_inv(max_n);
+int nck(int n, int k) {
+    return k < 0 ? 0 : n < 0 ? 0 : k > n ? 0 : fac[n] * fac_inv[k] % MOD * fac_inv[n - k] % MOD;
 };
+void precalbino() {
+    fac[0] = 1;
+    fore(i, 1, max_n)
+        fac[i] = fac[i - 1] * i % MOD;
+    fac_inv[max_n - 1] = invmod(fac[max_n - 1]);
+    for(int i = max_n - 2; i >= 0; i--)
+        fac_inv[i] = fac_inv[i + 1] * (i + 1) % MOD;
+}
