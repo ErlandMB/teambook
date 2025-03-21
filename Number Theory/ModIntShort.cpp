6ce069056400e8fd517fce1f3e111eac259d9d5c
ErlandMB <mauricioerland02@gmail.com>
2025-03-21 00:46:32 -0400

    Update ModIntShort.cpp

--- Number Theory/ModIntShort.cpp
+++ Number Theory/ModIntShort.cpp
@@ -1,30 +1,32 @@
-struct mi {
+const int MOD = 1e9 + 7;
+
+struct mint {
 	int v;
 	explicit operator int() const { return v; }
-	mi() { v = 0; }
-	mi(long long _v) : v(_v % MOD) { v += (v < 0) * MOD; }
+	mint() { v = 0; }
+	mint(long long _v) : v(_v % MOD) { v += (v < 0) * MOD; }
 };
-mi &operator+=(mi &a, mi b) {
+mint &operator+=(mint &a, mint b) {
 	if ((a.v += b.v) >= MOD) a.v -= MOD;
 	return a;
 }
-mi &operator-=(mi &a, mi b) {
+mint &operator-=(mint &a, mint b) {
 	if ((a.v -= b.v) < 0) a.v += MOD;
 	return a;
 }
-mi operator+(mi a, mi b) { return a += b; }
-mi operator-(mi a, mi b) { return a -= b; }
-mi operator*(mi a, mi b) { return mi((long long)a.v * b.v); }
-mi &operator*=(mi &a, mi b) { return a = a * b; }
-mi pow(mi a, long long p) {
+mint operator+(mint a, mint b) { return a += b; }
+mint operator-(mint a, mint b) { return a -= b; }
+mint operator*(mint a, mint b) { return mint((long long)a.v * b.v); }
+mint &operator*=(mint &a, mint b) { return a = a * b; }
+mint pow(mint a, long long p) {
 	assert(p >= 0);
 	return p == 0 ? 1 : pow(a * a, p / 2) * (p & 1 ? a : 1);
 }
-mi inv(mi a) {
+mint inv(mint a) {
 	assert(a.v != 0);
 	return pow(a, MOD - 2);
 }
-mi operator/(mi a, mi b) { return a * inv(b); }
+mint operator/(mint a, mint b) { return a * inv(b); }
 
 int main() {
 	{
@@ -32,7 +34,7 @@ int main() {
 		cout << (long long)a * b % MOD * c % MOD << "\n";  // 49000000
 	}
 	{
-		mi a = 1e8, b = 1e8, c = 1e8;
+		mint a = 1e8, b = 1e8, c = 1e8;
 		// cout << a * b * c << "\n";  // Errors- we have to cast this an an int
 		cout << (int)(a * b * c) << "\n";  // 49000000
 	}
