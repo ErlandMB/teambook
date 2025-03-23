5b894f81cbaa8f25fe164296aa80070e2d58b35e
ErlandMB <mauricioerland02@gmail.com>
2025-03-23 18:15:36 -0400

    Update Floyd-Warshall.cpp

--- Graphs/Shortest Paths/Floyd-Warshall.cpp
+++ Graphs/Shortest Paths/Floyd-Warshall.cpp
@@ -1,13 +1,13 @@
 // Floyd-Warshall algorithm
-	for (int k = 0; k < n; k++) {
-		for (int i = 0; i < n; i++) {
-			for (int j = i + 1; j < n; j++) {
-				long long new_dist = min_dist[i][k] + min_dist[k][j];
-				if (new_dist < min_dist[i][j]) {
-					min_dist[i][j] = min_dist[j][i] = new_dist;
-				}
+for (int k = 0; k < n; k++) {
+	for (int i = 0; i < n; i++) {
+		for (int j = i + 1; j < n; j++) {
+			long long new_dist = min_dist[i][k] + min_dist[k][j];
+			if (new_dist < min_dist[i][j]) {
+				min_dist[i][j] = min_dist[j][i] = new_dist;
 			}
 		}
+	}
   }
 // Given a directed or an undirected weighted graph G with n vertices. The task is to find the length of the shortest path between each pair of vertices i and j .
 
