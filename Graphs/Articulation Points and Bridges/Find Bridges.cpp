e81cbc508e17582363c5023b40f452286b02991b
ErlandMB <mauricioerland02@gmail.com>
2025-08-06 09:07:06 -0400

    Update Find Bridges.cpp

--- Graphs/Articulation Points and Bridges/Find Bridges.cpp
+++ Graphs/Articulation Points and Bridges/Find Bridges.cpp
@@ -1,14 +1,16 @@
 void IS_BRIDGE(int v,int to); // some function to process the found bridge
-int n; // number of nodes
-vector<vector<int>> adj; // adjacency list of graph
+ll n; // number of nodes
+vvl adj; // adjacency list of graph
 
 vector<bool> visited;
 vector<int> tin, low;
+vl subsize;
 int timer;
 
 void dfs(int v, int p = -1) {
     visited[v] = true;
     tin[v] = low[v] = timer++;
+    subsize[v] = 1;
     bool parent_skipped = false;
     for (int to : adj[v]) {
         if (to == p && !parent_skipped) {
@@ -20,8 +22,12 @@ void dfs(int v, int p = -1) {
         } else {
             dfs(to, v);
             low[v] = min(low[v], low[to]);
-            if (low[to] > tin[v])
+            subsize[v] += subsize[to];
+            if (low[to] > tin[v]) {
                 IS_BRIDGE(v, to);
+                ll a = subsize[to];
+                ll b = n - a;
+            }
         }
     }
 }
@@ -31,6 +37,7 @@ void find_bridges() {
     visited.assign(n, false);
     tin.assign(n, -1);
     low.assign(n, -1);
+    subsize.assign(n, 0);
     for (int i = 0; i < n; ++i) {
         if (!visited[i])
             dfs(i);
