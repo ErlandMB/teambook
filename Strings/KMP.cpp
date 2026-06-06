21774ab127253d581a8552fab6c52848f7967629
ErlandMB <mauricioerland02@gmail.com>
2026-06-06 19:53:10 -0400

    Fix variable declaration style in KMP.cpp

--- Strings/KMP.cpp
+++ Strings/KMP.cpp
@@ -1,8 +1,8 @@
 //Knuth-Morris-Pratt
 //pi[i] = guarda el mejor borde de cada prefijo
 vector<ll> pi(const string &s) {
-	  ll n=sz(s);
-    vl pi_s(n);
+	ll n=sz(s);
+    vector<ll> pi_s(n);
     for(int i=1, j=0; i<n; i++){
         while(j>0&&s[i]!=s[j])j=pi_s[j-1];
         if(s[i]==s[j])j++;
