4064714c97d599c82d7d83234d2ba0f868c138d8
ErlandMB <mauricioerland02@gmail.com>
2026-06-06 19:50:18 -0400

    Update z_algorithm.cpp

--- Strings/z_algorithm.cpp
+++ Strings/z_algorithm.cpp
@@ -1,17 +1,17 @@
-vector<int> z_function(const string &s) {
-  	vector<int> z(s.size());
-  	z[0] = s.size();
-  
-  	int l = 0;
-  	int r = 0;
-  	for (int i = 1; i < s.size(); i++) {
-  		z[i] = max(0, min(z[i - l], r - i + 1));
-  		while (i + z[i] < s.size() && s[z[i]] == s[i + z[i]]) {
-  			l = i;
-  			r = i + z[i];
+//El vector z[i]=cuantos caracteres coinciden entre s[0...] y s[i...]
+//es decir cuanto se parece cada posicion al inicio de la cadena
+vector<ll> z_function(const string &s){
+  	vector<ll> z(s.size());
+  	z[0]=s.size();
+  	ll l=0;
+  	ll r=0;
+  	for(int i=1; i<s.size(); i++){
+  		z[i]=max(0LL, min(z[i-l], r-i+1));
+  		while(i+z[i]<s.size()&&s[z[i]]==s[i+z[i]]){
+  			l=i;
+  			r=i+z[i];
   			z[i]++;
   		}
   	}
-  
-	  return z;
+    return z;
 }
