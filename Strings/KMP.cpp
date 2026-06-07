f9e9c1b2f9451c1507eadaf8d63c6d30b5c7f514
ErlandMB <mauricioerland02@gmail.com>
2026-06-06 22:07:19 -0400

    Update KMP.cpp

--- Strings/KMP.cpp
+++ Strings/KMP.cpp
@@ -1,6 +1,6 @@
 //Knuth-Morris-Pratt
 //pi[i] = guarda el mejor borde de cada prefijo
-vector<ll> pi(const string &s) {
+vector<ll> kmp(const string &s) {
 	ll n=sz(s);
     vector<ll> pi_s(n);
     for(int i=1, j=0; i<n; i++){
